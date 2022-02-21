f792cdb6e2a5f88a46d5fbdc447b354c96676634
John Nguyen <36948494+nguyening20@users.noreply.github.com>
2022-02-21 15:58:43 -0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,39 @@
+from crypt import methods
+from flask import Flask, send_from_directory
+from apscheduler.schedulers.background import BackgroundScheduler
+from medium import mediumPostsHandler, getPosts
+import os
+
+app = Flask(__name__)
+
+scheduler = BackgroundScheduler()
+# run job every day at midnight
+scheduler.add_job(mediumPostsHandler, 'cron', month='*',
+                  day='*', hour='0')
+scheduler.start()
+
+# catch all paths
+
+app = Flask(__name__, static_folder='client/build')
+
+
+# api paths
+
+
+@app.route('/api/posts', methods=['GET'])
+def apiPaths():
+    posts = getPosts()
+    return {"data": posts}
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>', methods=['GET', 'POST'])
+def allPaths(path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True, use_debugger=False, use_reloader=True)
---
+++ medium.py
@@ -0,0 +1,68 @@
+import requests
+import json
+import os
+postsPath = "posts.json"
+
+
+def formatPost(post):
+    s = post['description'].index("<p>")
+    e = post['description'].index("</p>")
+    preview = post['description'][s:e]
+    preview = preview.replace("<strong>", "")
+    preview = preview.replace("</strong>", "")
+    h4s = post['description'].index("<h4>")
+    h4e = post['description'].index("</h4>")
+    h4 = post['description'][h4s:h4e]
+    post['description'] = h4 + "</h4>" + preview[0:200] + "...</p>"
+    r = post["guid"].split("https://medium.com/p/")
+    post['id'] = r[1]
+
+
+def savePosts(posts):
+    with open(postsPath, "w") as file:
+        json.dump(posts, file)
+
+
+def getPosts():
+    if os.path.isfile(postsPath) and os.access(postsPath, os.R_OK):
+        with open(postsPath) as file:
+            return json.load(file)
+    else:
+        return []
+
+
+def findIndex(pred, iterable):
+    for x in range(len(iterable)):
+        ele = iterable[x]
+        if pred(ele):
+            return x
+    return -1
+
+
+def mediumPostsHandler():
+    # get old posts in store
+    print("started")
+    postStore = getPosts()
+    res = requests.get(
+        "https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@athletecoder")
+    obj = res.json()
+    posts = obj['items']
+    # reformat
+    for post in posts:
+        formatPost(post)
+    # update postStore
+
+    if len(postStore) == 0:
+        postStore.extend(posts)
+    else:
+        for p in posts:
+            matchIndex: int = findIndex(
+                lambda item: item.get("id") == p.get("id"), postStore)
+            if matchIndex > -1:
+                postStore[matchIndex] = p
+            else:
+                postStore.extend(p)
+
+    # save posts
+    savePosts(postStore)
+    print("updated")
